178f49d62bd2f2a13a734bc9387f54a62e1c4d14
asnowmang <wsobs@yandex.com>
2017-11-26 06:10:10 -0600

    Added formatting to print the numbers on the same line as the previous text output

--- ppicalc.f
+++ ppicalc.f
@@ -1,14 +1,18 @@
-      PROGRAM PPI Calc
-c     Calculates pixels-per-inch
-
+      PROGRAM PPICALC
+c     Calculates pixels-per-unit
       REAL w, h, size
-      
-      WRITE (*,*) 'PIXEL WIDTH:'
-      READ (*,*) w
-      WRITE (*,*) 'PIXEL HEIGHT:'
-      READ (*,*) h
-      WRITE (*,*) 'SIZE:'
-      READ (*,*) size
-      WRITE (*,*) 'PPI: ', SQRT(w**2 + h**2) / size
 
-      END PROGRAM PPI Calc
+c     For output without a newline
+ 100  FORMAT(A, $)
+c     To format PPI output
+ 200  FORMAT(A5, I3)
+
+      WRITE (*, 100), 'PIXEL WIDTH: '
+      READ (*,*), w
+      WRITE (*, 100), 'PIXEL HEIGHT: '
+      READ (*,*) h
+      WRITE (*, 100), 'SCREEN SIZE: '
+      READ (*,*) size
+      WRITE (*, 200), 'PPI: ', NINT(SQRT(w**2 + h**2) / size)
+      
+      END PROGRAM PPICALC
